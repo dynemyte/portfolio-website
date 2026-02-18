050e4b8287d718e2a00d50c8206340b8c704fcab
dynemyte <danielacp@ymail.com>
2026-02-18 15:51:31 +0000

    Add JSON data display to Alerts, Operations, and Operatives pages

--- src/modules/alerts/containers/Alerts.jsx
+++ src/modules/alerts/containers/Alerts.jsx
@@ -1,5 +1,27 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box } from '@chakra-ui/react';
+import alertsData from '../../../data/alerts.json';
 
 export default function Alerts() {
-  return <Heading as="h1" size="xl">Alerts Page</Heading>;
+  return (
+    <div>
+      <Heading as="h1" size="xl">Alerts Page</Heading>
+      <Box mt={6} p={5} bg="gray.50" borderRadius="md">
+        <Heading as="h2" size="lg" mb={4}>Alerts Data (JSON)</Heading>
+        <Box 
+          as="pre" 
+          fontSize="xs" 
+          whiteSpace="pre-wrap" 
+          overflow="auto" 
+          maxH="400px"
+          bg="white"
+          p={4}
+          borderRadius="md"
+          border="1px solid"
+          borderColor="gray.200"
+        >
+          {JSON.stringify(alertsData, null, 2)}
+        </Box>
+      </Box>
+    </div>
+  );
 }
--- src/modules/operatives/containers/Operatives.jsx
+++ src/modules/operatives/containers/Operatives.jsx
@@ -1,5 +1,27 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box } from '@chakra-ui/react';
+import operativesData from '../../../data/operatives.json';
 
 export default function Operatives() {
-  return <Heading as="h1" size="xl">Operatives Page</Heading>;
+  return (
+    <div>
+      <Heading as="h1" size="xl">Operatives Page</Heading>
+      <Box mt={6} p={5} bg="gray.50" borderRadius="md">
+        <Heading as="h2" size="lg" mb={4}>Operatives Data (JSON)</Heading>
+        <Box 
+          as="pre" 
+          fontSize="xs" 
+          whiteSpace="pre-wrap" 
+          overflow="auto" 
+          maxH="400px"
+          bg="white"
+          p={4}
+          borderRadius="md"
+          border="1px solid"
+          borderColor="gray.200"
+        >
+          {JSON.stringify(operativesData, null, 2)}
+        </Box>
+      </Box>
+    </div>
+  );
 }
