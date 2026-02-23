afa3e12a537e208c79043ec5c627666b93e0524b
dynemyte <danielacp@ymail.com>
2026-02-23 16:55:49 +0000

    Refactor Operatives page by removing old layout and implementing OperativesGrid component for better data presentation; add Prettier configuration for code formatting

--- src/modules/operatives/containers/Operatives.jsx
+++ src/modules/operatives/containers/Operatives.jsx
@@ -1,27 +1,11 @@
-import { Heading, Box } from '@chakra-ui/react';
-import { operatives as operativesData } from '../../../data';
+import { Heading, Box } from '@chakra-ui/react'
+import { operatives as operativesData } from '../../../data'
+import { OperativesGrid } from '../components'
 
 export default function Operatives() {
   return (
     <div>
-      <Heading as="h1" size="xl">Operatives Page</Heading>
-      <Box mt={6} p={5} bg="gray.50" borderRadius="md">
-        <Heading as="h2" size="lg" mb={4}>Operatives Data (JSON)</Heading>
-        <Box 
-          as="pre" 
-          fontSize="xs" 
-          whiteSpace="pre-wrap" 
-          overflow="auto" 
-          maxH="400px"
-          bg="white"
-          p={4}
-          borderRadius="md"
-          border="1px solid"
-          borderColor="gray.200"
-        >
-          {JSON.stringify(operativesData, null, 2)}
-        </Box>
-      </Box>
+      <OperativesGrid operatives={operativesData} />
     </div>
-  );
+  )
 }
