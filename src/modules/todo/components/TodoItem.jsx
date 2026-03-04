41381a21552f6ead1e407c32f2e68b2404f54b77
dynemyte <danielacp@ymail.com>
2026-03-04 18:12:12 +0000

    Refactor TodoItem component to use HStack and Checkbox for improved interactivity and styling

--- src/modules/todo/components/TodoItem.jsx
+++ src/modules/todo/components/TodoItem.jsx
@@ -1,18 +1,32 @@
-import { Box, Card } from '@chakra-ui/react'
+import { Card, HStack, Text, Checkbox } from '@chakra-ui/react'
 
 export default function TodoItem({ todo, onToggle }) {
   return (
-    <Card.Root
-      onClick={() => onToggle(todo.id)}
-      cursor="pointer"
-      userSelect="none"
-      _hover={{ bg: 'gray.50' }}
-      transition="0.2s"
-      borderRadius="md"
-    >
-      <Card.Body>
-        {todo.completed ? '✓ ' : '○ '}
-        {todo.text}
+    <Card.Root>
+      <Card.Body _hover={{ bg: 'gray.50' }}>
+        <HStack
+          gap={3}
+          align="center"
+          p={4}
+          onClick={() => onToggle(todo.id)}
+          //   If you later add a delete icon/button inside, it will also toggle. Fix later by adding a separate onClick handler for the delete button and calling event.stopPropagation() there.
+          cursor="default"
+          userSelect="none"
+          transition="background-color 0.2s ease"
+        >
+          <Checkbox.Root
+            checked={todo.completed}
+            onCheckedChange={e => {
+              e.stopPropagation()
+              onToggle(todo.id)
+            }}
+          >
+            <Checkbox.Control />
+            <Checkbox.HiddenInput />
+          </Checkbox.Root>
+
+          <Text flex="1">{todo.text}</Text>
+        </HStack>
       </Card.Body>
     </Card.Root>
   )
